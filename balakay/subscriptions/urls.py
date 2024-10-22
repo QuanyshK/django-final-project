ef2d0d82026fe879c535e505defaa43141e07055
AlikhanovY <elnur20050205@gmail.com>
2024-10-22 23:09:38 +0500

    sub

---
+++ balakay/subscriptions/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    SubscriptionListView,
+    SubscriptionDetailView,
+    SubscriptionCreateView,
+    SubscriptionUpdateView,
+    SubscriptionDeleteView
+)
+
+urlpatterns = [
+    path('subscriptions/', SubscriptionListView.as_view(), name='subscription-list'),
+    path('subscriptions/<int:pk>/', SubscriptionDetailView.as_view(), name='subscription-detail'),
+    path('subscriptions/new/', SubscriptionCreateView.as_view(), name='subscription-create'),
+    path('subscriptions/<int:pk>/edit/', SubscriptionUpdateView.as_view(), name='subscription-update'),
+    path('subscriptions/<int:pk>/delete/', SubscriptionDeleteView.as_view(), name='subscription-delete'),
+]
--- balakay/subscriptions/views.py
+++ balakay/subscriptions/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Subscription
 
-# Create your views here.
+
+class SubscriptionListView(ListView):
+    model = Subscription
+    template_name = 'subscriptions/subscription_list.html'  
+    context_object_name = 'subscriptions'
+
+class SubscriptionDetailView(DetailView):
+    model = Subscription
+    template_name = 'subscriptions/subscription_detail.html'
+    context_object_name = 'subscription'
+
+
+class SubscriptionCreateView(CreateView):
+    model = Subscription
+    template_name = 'subscriptions/subscription_form.html'
+    fields = ['active', 'name', 'price', 'duration_text', 'age_group', 'total_days', 'freeze_days', 'daily_visits', 'monthly_visits', 'premium_monthly_visits']
+
+
+class SubscriptionUpdateView(UpdateView):
+    model = Subscription
+    template_name = 'subscriptions/subscription_form.html'
+    fields = ['active', 'name', 'price', 'duration_text', 'age_group', 'total_days', 'freeze_days', 'daily_visits', 'monthly_visits', 'premium_monthly_visits']
+
+class SubscriptionDeleteView(DeleteView):
+    model = Subscription
+    template_name = 'subscriptions/subscription_confirm_delete.html'
+    success_url = reverse_lazy('subscription-list')
