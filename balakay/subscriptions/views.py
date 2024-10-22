d94a28836ff5a2b9c7d5abd8e909c1189b446c1f
AlikhanovY <elnur20050205@gmail.com>
2024-10-22 23:11:11 +0500

    fix

--- balakay/subscriptions/views.py
+++ balakay/subscriptions/views.py
@@ -3,30 +3,3 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy
 from .models import Subscription
 
-
-class SubscriptionListView(ListView):
-    model = Subscription
-    template_name = 'subscriptions/subscription_list.html'  
-    context_object_name = 'subscriptions'
-
-class SubscriptionDetailView(DetailView):
-    model = Subscription
-    template_name = 'subscriptions/subscription_detail.html'
-    context_object_name = 'subscription'
-
-
-class SubscriptionCreateView(CreateView):
-    model = Subscription
-    template_name = 'subscriptions/subscription_form.html'
-    fields = ['active', 'name', 'price', 'duration_text', 'age_group', 'total_days', 'freeze_days', 'daily_visits', 'monthly_visits', 'premium_monthly_visits']
-
-
-class SubscriptionUpdateView(UpdateView):
-    model = Subscription
-    template_name = 'subscriptions/subscription_form.html'
-    fields = ['active', 'name', 'price', 'duration_text', 'age_group', 'total_days', 'freeze_days', 'daily_visits', 'monthly_visits', 'premium_monthly_visits']
-
-class SubscriptionDeleteView(DeleteView):
-    model = Subscription
-    template_name = 'subscriptions/subscription_confirm_delete.html'
-    success_url = reverse_lazy('subscription-list')
