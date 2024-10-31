2e82d089894b41c6315759ef736b79a5baef5714
QuanyshK <zoomer.qurbany@gmail.com>
2024-11-01 00:30:09 +0500

    Home, Pagination
    Update home page: search from home, delete center list, add pagination for all pages with list, improve favorites

--- balakay/centers/views.py
+++ balakay/centers/views.py
@@ -1,32 +1,61 @@
-from datetime import timezone
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-from .models import Section, Center, Schedule, Category, Booking, FavoriteSection
-from users.models import Client
-from .forms import BookingForm
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-# Create your views here.
+from .models import Section, Center, Schedule, Category, Booking, FavoriteSection
+from .forms import BookingForm
 
 def home_view(request):
-    categories = Category.objects.prefetch_related('sections').all()
-    
-    return render(request, 'centers/home.html', {'categories': categories})
+    query = request.GET.get("q", "")
+    search_type = request.GET.get("search_type", "all")
+    category_id = request.GET.get("category", "")
 
+    centers = Center.objects.none()
+    sections = Section.objects.none()
+    categories = Category.objects.all()
+
+    if search_type == "centers" or search_type == "all":
+        centers = Center.objects.filter(name__icontains=query)
+
+    if search_type == "sections" or search_type == "all":
+        sections = Section.objects.filter(name__icontains=query)
+        if category_id:
+            sections = sections.filter(category__id=category_id)
+    centers_paginator = Paginator(centers, 10)  
+    centers_page_number = request.GET.get('page')
+    centers_page = centers_paginator.get_page(centers_page_number)
+    sections_paginator = Paginator(sections, 10) 
+    sections_page_number = request.GET.get('page')
+    sections_page = sections_paginator.get_page(sections_page_number)
+
+    return render(request, "centers/home.html", {
+        "query": query,
+        "search_type": search_type,
+        "category_id": category_id,
+        "centers": centers_page,
+        "sections": sections_page,
+        "categories": categories
+    })
 def section_view(request, id):
     section = get_object_or_404(Section, id=id)
-    schedules = Schedule.objects.filter(section=section, start_time__gte=timezone.now())
-    return render(request, 'centers/section.html', {"section": section, "schedules": schedules})
+    schedules = Schedule.objects.filter(section=section, start_time__gte=timezone.now()).order_by('start_time')
+    paginator = Paginator(schedules, 5)
+    is_favorited = False
+    if request.user.is_authenticated:
+        is_favorited = request.user.client.favoritesection_set.filter(section=section).exists()
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'centers/section.html', {"section": section, "page_obj": page_obj, "schedules": schedules, 'is_favorited': is_favorited,})
 
 def book_schedule_view(request, schedule_id):
     schedule = get_object_or_404(Schedule, id=schedule_id)
     existing_bookings = Booking.objects.filter(schedule=schedule, user=request.user)
-
     if request.method == 'POST':
-        form = BookingForm(request.POST, user=request.user, section=schedule.section) 
+        form = BookingForm(request.POST, user=request.user, section=schedule.section)
         if form.is_valid():
-            if existing_bookings.filter(user=request.user).exists():
+            if existing_bookings.exists():
                 form.add_error(None, 'You have already booked this schedule for this child.')
             else:
                 booking = form.save(commit=False)
@@ -35,56 +64,40 @@ def book_schedule_view(request, schedule_id):
                 booking.save()
                 schedule.total_slots -= 1
                 schedule.save()
-
                 return redirect(reverse('booking_success'))
     else:
-        form = BookingForm(user=request.user, section=schedule.section)  # Передаем секцию
-
+        form = BookingForm(user=request.user, section=schedule.section)
     return render(request, 'centers/book_schedule.html', {'form': form, 'schedule': schedule})
 
 def booking_success_view(request):
     return render(request, 'centers/booking_success.html')
 
-
-
 def cancel_booking_view(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id)
-
     if booking.status == Booking.PENDING:
         booking.status = Booking.CANCELLED
         booking.cancelled_at = timezone.now()
         booking.save()
-
     return redirect('my-schedule')
-
-def center_list_view(request):
-    query = request.GET.get('q', '')
-
-    centers = Center.objects.all()
-    if query:
-        centers = centers.filter(Q(name__icontains=query) | Q(id__icontains=query))
-
-    return render(request, 'centers/center_list.html', {
-        'centers': centers,
-        'query': query,
-    })
 @login_required
 def user_bookings(request):
-    bookings = Booking.objects.filter(user=request.user, schedule__start_time__gte=timezone.now()).select_related('schedule')
-    return render(request, 'centers/my_schedule.html', {'bookings': bookings})
+    bookings = Booking.objects.filter(user=request.user, schedule__start_time__gte=timezone.now()).select_related('schedule').order_by('schedule__start_time')
+    paginator = Paginator(bookings, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'centers/my_schedule.html', {'page_obj': page_obj, 'bookings': bookings})
 
 def center_details(request, center_id):
     center = get_object_or_404(Center, id=center_id)
-    schedules = Schedule.objects.filter(section__center=center)
-
-    return render(request, 'centers/center_details.html', {'center': center, 'schedules': schedules})
-
+    sections = Section.objects.filter(center=center).order_by('name')
+    paginator = Paginator(sections, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'centers/center_details.html', {'center': center, 'page_obj': page_obj, 'sections': sections})
 
 def booking_detail(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id, user=request.user)
     schedule = booking.schedule
-
-
     if request.method == 'POST':
         action = request.POST.get('action')
         if action == 'confirm':
@@ -95,47 +108,45 @@ def booking_detail(request, booking_id):
             booking.cancelled_at = timezone.now()
             schedule.total_slots += 1
             schedule.save()
-
         booking.save()
         return redirect(reverse('booking_detail', args=[booking.id]))
-
     return render(request, 'centers/booking_detail.html', {'booking': booking})
-
 
 @login_required
 def add_favorite_section(request, id):
     section = get_object_or_404(Section, id=id)
     client = request.user.client
-
-    favorite, created = FavoriteSection.objects.get_or_create(client=client, section=section)
-
+    favorite, created = FavoriteSection.objects.get_or_create(client=request.user.client, section=section)
+    
     if created:
-        message = "Section added to favorites!"
+        return redirect('section', id=id)
     else:
-        message = "Section is already in favorites."
-
-    return redirect('section', id=id)
-
+        favorite.delete()
+        return redirect('section', id=id)
 
 @login_required
 def remove_favorite_section(request, id):
     section = get_object_or_404(Section, id=id)
     client = request.user.client
     FavoriteSection.objects.filter(client=client, section=section).delete()
-    return redirect('favorite_sections') 
+    return redirect('favorite_sections')
 
 @login_required
 def favorite_sections_view(request):
     client = request.user.client
-    favorite_sections = FavoriteSection.objects.filter(client=client).select_related('section')
-
-    return render(request, 'centers/favorite_sections.html', {'favorite_sections': favorite_sections})
+    favorite_sections = FavoriteSection.objects.filter(client=client).select_related('section').order_by('section__name')
+    paginator = Paginator(favorite_sections, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'centers/favorite_sections.html', {'page_obj': page_obj, 'favorite_sections': favorite_sections})
 
 @login_required
 def past_bookings_view(request):
     past_bookings = Booking.objects.filter(
         user=request.user,
         schedule__start_time__lt=timezone.now()
-    ).order_by('-schedule__start_time')  
-
-    return render(request, 'centers/past_bookings.html', {'past_bookings': past_bookings})
+    ).order_by('-schedule__start_time')
+    paginator = Paginator(past_bookings, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'centers/past_bookings.html', {'page_obj': page_obj, 'past_bookings': past_bookings})
